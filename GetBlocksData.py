09264ba328a951a38a6e99af2f4018b8b496a200
Orhan AKDOĞAN <orhan2akdogan@gmail.com>
2025-01-09 17:13:50 +0300

    Create GetBlocksData.py

---
+++ GetBlocksData.py
@@ -0,0 +1,45 @@
+#!pip install requests
+import requests
+
+def get_block_data(block_height):
+    """
+    Belirtilen blok yüksekliğindeki blok verilerini çeker.
+    """
+    url = f"https://blockchain.info/block-height/{block_height}?format=json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data["blocks"][0]
+    else:
+        print(f"Hata: {response.status_code}")
+        return None
+
+def get_block_info(start_block=0, end_block=100):
+    """
+    Belirtilen blok aralığındaki adres ve public key bilgilerini çeker.
+    """
+    block_data = []
+    for height in range(start_block, end_block):
+        block = get_block_data(height)
+        if block:
+            block_info = {"block": height, "addresses": []}
+            for tx in block["tx"]:
+                for out in tx["out"]:
+                    if "addr" in out and "script" in out:
+                        address_info = {
+                            "address": out["addr"],
+                            "public_key": out["script"]
+                        }
+                        block_info["addresses"].append(address_info)
+            block_data.append(block_info)
+    return block_data
+
+# İlk 100 blok için verileri çek
+block_info_list = get_block_info(1, 100) # max 4800
+
+# Formatlı çıktı yazdır
+for block_info in block_info_list:
+    for address in block_info["addresses"]:
+        print(f"{block_info['block']}\t" + f"{address['address']}\t"+ f"{address['public_key']}")
+
+
